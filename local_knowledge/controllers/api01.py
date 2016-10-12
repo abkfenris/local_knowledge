de4ba3eb596f079e9af98795fb9f463366f6524c
Alex Kerney <abk@mac.com>
2016-10-12 07:48:32 -0400

    Added OSM data to Way and Node geojson

--- local_knowledge/controllers/api01.py
+++ local_knowledge/controllers/api01.py
@@ -13,18 +13,25 @@ def index():
 @api.route('/nodes')
 def nodes():
     """ Returns a GeoJSON Feature collection with all nodes """
-    results = db.session.query(Node.id, Node.geom.ST_AsGeoJSON().cast(JSON).label('geom')).all()
+    results = db.session.query(Node.id, Node.json, Node.geom.ST_AsGeoJSON().cast(JSON).label('geom')).all()
     fc = {"type": "FeatureCollection",
-          "features": [dict(res.geom, properties={'id': res.id}) for res in results]}
+          "features": [dict(res.geom, 
+                            properties={'id': res.id, 
+                                        'osm_data': {k:res.json[k] for k in res.json if k!='nd'}
+                                        }) for res in results]}
     return jsonify(fc)
 
 
 @api.route('/ways')
 def ways():
     """ Returns a GeoJSON Feature collection with all ways """
-    results = db.session.query(Way.id, Way.name, Way.geom.ST_AsGeoJSON().cast(JSON).label('geom')).all()
+    results = db.session.query(Way.id, Way.name, Way.json, Way.geom.ST_AsGeoJSON().cast(JSON).label('geom')).all()
     fc = {
         'type': 'FeatureCollection',
-        'features': [dict(res.geom, properties={'id': res.id, 'name': res.name}) for res in results]
+        'features': [dict(res.geom, 
+                          properties={'id': res.id, 
+                                      'name': res.name, 
+                                      'osm_data': {k:res.json[k] for k in res.json if k!='linestring' and k!='nd'} 
+                                      }) for res in results]
     }
     return jsonify(fc)
